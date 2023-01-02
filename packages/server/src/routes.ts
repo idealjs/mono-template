cbfcfe9c4e048cbaac401c60c89a7b63afb29bc0
cqh963852 <cqh963852@hotmail.com>
2023-01-02 14:33:09 +0800

    feat: use esm

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -1,9 +1,11 @@
+import { moduleA } from "@idealjs/mono-template";
 import Router from "@koa/router";
 
 const router = new Router();
 
 router.get("/health", (ctx, next) => {
   ctx.body = { alive: true };
+  console.log("[test] moduleA", moduleA);
   next();
 });
 
